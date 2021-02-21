ce302e4edf9d990e00977f109a38b5fc4e1871aa
Louis Cordier <lcordier@gmail.com>
2021-02-21 16:36:38 +0200

    "Simpler" solution.

--- backup.py
+++ backup.py
@@ -4,14 +4,29 @@
 """
 import csv
 import datetime
+import json
 import logging
 import logging.config
 import optparse
 import os
+from pprint import pprint
 from shlex import quote
 import subprocess
 import sys
 
+
+# https://serverfault.com/questions/470046/rsync-from-linux-host-to-fat32
+PARAMS = '-rtD --modify-window=1 --size-only'
+
+# Add (params, src, dst) 3-tuples to the BACKUPS list.
+# All paths should end in a /
+# ~ is expanded to the user's home directory.
+BACKUPS = [
+    (PARAMS, '/tmp/logs/', 'backup/logs/%w/'),
+    (PARAMS, '~/zettelkasten/', 'backup/zettelkasten/%w/'),
+    (PARAMS, '~/.ssh/', 'backup/.ssh/%w/'),
+    (PARAMS, '~/tax/', 'backup/tax/%w/'),
+]
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,7 +44,7 @@ LOGGING_CONFIG = {
             'class': 'logging.NullHandler',
             'level': 'NOTSET',
         },
-        'file': {
+            'file': {
             'class': 'logging.handlers.RotatingFileHandler',
             'level': 'INFO',
             'formatter': 'summary',
@@ -59,17 +74,62 @@ logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger()
 
 
-def exec_(cmd, params, src, dst):
-    """ Execute command.
+def ensure_directory_exists(path, expand_user=True, file=False):
+    """ Create a directory if it doesn't exists.
+
+        Expanding '~' to the user's home directory on POSIX systems.
     """
-    if not dst:
-        command = "{cmd} {params} {src}".format(cmd=cmd, params=params, src=quote(src))
+    if expand_user:
+        path = os.path.expanduser(path)
+
+    if file:
+        directory = os.path.dirname(path)
     else:
-        command = "{cmd} {params} {src} {dst}".format(cmd=cmd, params=params, src=quote(src), dst=quote(dst))
+        directory = path
+
+    if not os.path.exists(directory) and directory:
+        try:
+            os.makedirs(directory)
+        except OSError as e:
+            # A parallel process created the directory after the existence check.
+            pass
+
+    return(path)
+
+
+def partitions():
+    """ Return {partition: mountpoint}.
+    """
+    d = {}
+    command = "lsblk -J"
+    r = json.loads(subprocess.check_output(command, shell=True))
+    for rec in r.get('blockdevices', []):
+        if rec.get('type') in ['disk']:
+            for child in rec.get('children', []):
+                if child.get('type') in ['part']:
+                    name = child.get('name')
+                    mountpoint = child.get('mountpoint')
+                    d[name] = mountpoint
+    return d
+
+
+def mount(device):
+    """ udiskie-mount the device.
+        https://pypi.org/project/udiskie/
+    """
+    command = f"udiskie-mount /dev/{device} >/dev/null 2>&1"
     subprocess.check_call(command, shell=True)
 
 
-def rsync(cmd, params, src, dst):
+def umount(device):
+    """ udiskie-umount the device.
+        https://pypi.org/project/udiskie/
+    """
+    command = f"udiskie-umount /dev/{device} >/dev/null 2>&1"
+    subprocess.check_call(command, shell=True)
+
+
+def rsync(params, src, dst):
     """ Execute rsync command.
     """
     # command = "rsync {params} {src} {dst} >/dev/null 2>&1".format(params=params, src=quote(src), dst=quote(dst))
@@ -77,66 +137,64 @@ def rsync(cmd, params, src, dst):
     subprocess.check_call(command, shell=True)
 
 
-def fail(cmd, params, src, dst):
-    """ Test the existance of dst.
-    """
-    if not os.path.exists(dst):
-        raise FailError('{} does not exist.'.format(dst))
-
-
-COMMANDS = {
-    'rsync': rsync,
-    'fail': fail,
-}
-
-
-class FailError(Exception):
-    pass
-
-
 if __name__ == '__main__':
 
     logger.info('Backup start: ' + ' '.join(sys.argv))
-
     parser = optparse.OptionParser()
-
-    parser.add_option('-c',
-                      '--config',
-                      dest='config',
+    parser.add_option('-d',
+                      '--device',
+                      dest='device',
                       action='store',
                       type='string',
                       default='',
-                      help='backup config file')
+                      help='storage device (partition)')
 
     options, args = parser.parse_args()
 
-    if not (options.config):
+    if not (options.device):
         parser.print_help()
         sys.exit()
 
     now = datetime.datetime.now()
+    device = os.path.basename(options.device)
 
-    config = open(options.config, 'r')
-    reader = csv.reader(config)
-    header = next(reader)
+    # Hack.
+    umount(device)
 
-    for idx, row in enumerate(reader, 2):
-        cmd, params, src, dst = [field.strip() for field in row]
+    partitions_ = partitions()
+    mountpoint = partitions_.get(device)
+    if mountpoint:
+        logger.error(f'Device {device} ready mounted on {mountpoint}')
+        sys.exit(f'Unmount {device} and try again.')
+    else:
+        if device not in partitions_:
+            logger.error(f'Device {device} not found.')
+            sys.exit(f'Is device {device} plugged in?')
+        else:
+            mount(device)
+            partitions_ = partitions()
+            mountpoint = partitions_.get(device)
+            logger.info(f'Device {device} mounted on {mountpoint}')
+
+    # Do rsync's.
+    for params, source, destination in BACKUPS:
         try:
+            src = os.path.expanduser(source)
             if '%' in src:
                 src = now.strftime(src)
 
+            dst = os.path.join(mountpoint, destination)
             if '%' in dst:
                 dst = now.strftime(dst)
 
-            func = COMMANDS.get(cmd.lower(), exec_)
-            if func:
-                func(cmd, params, src, dst)
+            if dst.endswith('/'):
+                ensure_directory_exists(dst)
             else:
-                logger.error('Unknown command, row={}: {}'.format(idx, ','.join(row)))
-        except FailError as e:
-            logger.error(e)
-            break
+                ensure_directory_exists(dst, file=True)
+
+            rsync(params, src, dst)
         except:
-            logger.exception('row={}: {}'.format(idx, ','.join(row)))
+            logger.exception()
+
+    logger.info('Backup done.')
 
